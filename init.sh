1c445da09baf4c0ce0a74e389630899cb9ab52b9
Justin Kaeser <justin.kaeser@jetbrains.com>
2019-06-18 13:48:11 +0200

    update, simplify

--- init.sh
+++ init.sh
@@ -2,11 +2,11 @@
 # coursier launch ch.epfl.scala:bloop-launcher_2.12:1.3.2 -- 1.3.2
 ROOTDIR=`pwd`
 
-LIBS="org.scala-lang:scala-library:2.12.8 org.scala-lang:scala-compiler:2.12.8 org.scala-lang:scala-reflect:2.12.8"
+LIBS="org.scala-lang:scala-library:2.13.0"
 JARS=$(bin/coursier fetch -r sonatype:releases $LIBS)
 CLASSPATH=$(echo $JARS | sed -e s/[[:blank:]]/\",\"/g)
 
-COMPILER_LIBS="org.scala-lang:scala-library:2.12.8 org.scala-lang:scala-compiler:2.12.8 org.scala-lang:scala-reflect:2.12.8"
+COMPILER_LIBS="org.scala-lang:scala-library:2.13.0 org.scala-lang:scala-compiler:2.13.0 org.scala-lang:scala-reflect:2.13.0"
 COMPILER_JARS=$(bin/coursier fetch -r sonatype:releases $COMPILER_LIBS)
 COMPILER_CLASSPATH=$(echo $COMPILER_JARS | sed -e s/[[:blank:]]/\",\"/g)
 
