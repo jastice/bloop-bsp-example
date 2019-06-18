9c5f63125d0424da486a68c99bddae8b1573fdba
Justin Kaeser <justin.kaeser@jetbrains.com>
2019-06-18 13:23:43 +0200

    initial

---
+++ init.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+# coursier launch ch.epfl.scala:bloop-launcher_2.12:1.3.2 -- 1.3.2
+ROOTDIR=`pwd`
+
+LIBS="org.scala-lang:scala-library:2.12.8 org.scala-lang:scala-compiler:2.12.8 org.scala-lang:scala-reflect:2.12.8"
+JARS=$(bin/coursier fetch -r sonatype:releases $LIBS)
+CLASSPATH=$(echo $JARS | sed -e s/[[:blank:]]/\",\"/g)
+
+COMPILER_LIBS="org.scala-lang:scala-library:2.12.8 org.scala-lang:scala-compiler:2.12.8 org.scala-lang:scala-reflect:2.12.8"
+COMPILER_JARS=$(bin/coursier fetch -r sonatype:releases $COMPILER_LIBS)
+COMPILER_CLASSPATH=$(echo $COMPILER_JARS | sed -e s/[[:blank:]]/\",\"/g)
+
+sed "s#\$ROOT#$ROOTDIR#;s#\$CLASSPATH#$CLASSPATH#;s#\$COMPILER_CLASSPATH#$COMPILER_CLASSPATH#" etc/build.json > .bloop/build.json
+
+# coursier launch ch.epfl.scala:bloop-launcher_2.12:1.3.2 -- 1.3.2
---
+++ src/Hi.scala
@@ -0,0 +1,7 @@
+package hi
+
+object Hi {
+    def main(args: Array[String]) = {
+        println("Hi!?")
+    }
+}
